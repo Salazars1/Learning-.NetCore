4d51918585e53bf8fd62df5b6a542fc29b85fc26
Santiago Salazar <salazars1@xavier.edu>
2018-10-15 14:04:34 -0400

    Add files via upload

---
+++ LearningHowToUee.NetCore/LearningHowToUee.NetCore/Controllers/HomeController1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LearningHowToUee.NetCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "I think this works, right?";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
